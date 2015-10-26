5247b2cff11f085b54e642cdf5ff9fa3f3abb116
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-10-26 20:08:10 -0300

    Add colums name, lasname, username to User

---
+++ db/migrate/20151026230714_add_colums_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :lastname, :string
+    add_column :users, :username, :string
+  end
+end
